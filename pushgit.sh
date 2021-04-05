af28264666fd9ce19d820289a8abfda8c1c8a86b
fhmlsml <email.gemail10@gmail.com>
2021-04-05 11:22:15 +0000

    dkit

--- pushgit.sh
+++ pushgit.sh
@@ -4,13 +4,22 @@ echo " =============== MANIPULATE THEN PUSH AGAIN =============== "
 
 sleep 2
 echo " =============== INSTALLING PYTHON3 PACKAGES =============== "
+
 sudo apt install python3-pip3
 sleep 2
 pip3 install -r requirements.txt
-echo " =============== INITIALIZE GIT REPO =============== "
+
+echo " =============== MANIPULATE AND PUSH =============== "
+
+sleep 3
+python3 manipulate.py
+sleep 2
+
+echo " =============== PUSHING TO GITHUB =============== "
+
 git add .
-sleep 1
 git commit -m "modifying file with automation"
 git push origin master
-touch .gitignore
-echo "hello" > ~/Desktop/new_file.txt
+
+
+echo " =============== DONE =============== "
